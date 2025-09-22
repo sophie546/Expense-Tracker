de3323455041ecea8c44808f2b3e6f4b6259085d
seobi <sophie.aloria@gmail.com>
2025-09-23 04:39:04 +0800

    feature(expense_tracker): setup static files

--- expense_tracker_project/expense_tracker_project/settings.py
+++ expense_tracker_project/expense_tracker_project/settings.py
@@ -115,7 +115,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Optional: tells Django where to collect static files when running collectstatic
+STATICFILES_DIRS = [
+    BASE_DIR / 'expense_tracker_project' / 'static',  # your app-level static folder
+]
+
+# Optional: folder for collectstatic
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
